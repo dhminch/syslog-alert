f3777094c7cd2407fac2fe557a12cc9a7d8eae25
dhminch <dhminch@gmail.com>
2022-01-14 23:43:01 -0500

    Initial configuration handler and template

---
+++ Configuration.py
@@ -0,0 +1,31 @@
+
+from distutils.command.config import config
+import json
+import os
+import os.path
+
+import debug
+
+class Configuration:
+
+    def __init__(self, file):
+        if not os.path.isfile(file):
+            debug.log("Configuration file {} does not exist!".format(file))
+            exit(1)
+        if not os.access(file, os.O_RDONLY):
+            debug.log("Configuration file {} is not readable!".format(file))
+            exit(1)
+
+        with open(file, "r", encoding="utf-8") as handle:
+            try:
+                config_obj = json.load(handle)
+            except Exception as e:
+                debug.log("Configuration file {} is not valid JSON!".format(file))
+                raise e
+            
+        self.logs = config_obj["logs"]
+        self.cellphone = config_obj["cellphone"]
+        self.ratelimit = config_obj["ratelimit"]
+        self.twilio_messagingservice_sid = config_obj["twilio"]["messagingservice_sid"]
+        self.twilio_account_sid = config_obj["twilio"]["account_sid"]
+        self.twilio_auth_token = config_obj["twilio"]["auth_token"]
