63813f6c5923c588530277216daa2ed347afc0c2
dhminch <dhminch@gmail.com>
2022-01-15 15:28:34 -0500

    Add space in log output

--- debug.py
+++ debug.py
@@ -9,12 +9,12 @@ LEVEL = 3
 
 def error(msg):
     if DEBUGGING and LEVEL >= 1:
-        print("[E] {}:{}".format(datetime.now(), msg))
+        print("[E] {}: {}".format(datetime.now(), msg))
 
 def log(msg):
     if DEBUGGING and LEVEL >= 2:
-        print("[L] {}:{}".format(datetime.now(), msg))
+        print("[L] {}: {}".format(datetime.now(), msg))
 
 def debug(msg):
     if DEBUGGING and LEVEL >= 3:
-        print("[D] {}:{}".format(datetime.now(), msg))
+        print("[D] {}: {}".format(datetime.now(), msg))
