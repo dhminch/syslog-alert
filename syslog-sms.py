019a56998334102d2637f106cec33ecec047a74c
dhminch <dhminch@gmail.com>
2022-01-15 15:56:09 -0500

    Add handling for SIGINT

--- syslog-sms.py
+++ syslog-sms.py
@@ -1,12 +1,18 @@
 
 import os.path
+from signal import signal, SIGINT
 
 from Alarm import AlarmDispatcher
 from Configuration import Configuration
 from Debug import Debug
 from LogMonitor import LogMonitor
 
+def sigint_handler(signal_received, frame):
+    print("Exiting now!")
+    exit(0)
+
 def main():
+    signal(SIGINT, sigint_handler)
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     config = Configuration(os.path.join(__location__, "config.json"))
     Debug.set_verbosity(config.logging_verbosity)
