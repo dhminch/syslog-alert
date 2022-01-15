4f912acbfa4e11a5877a9f7721d1a2835cc6eb1e
dhminch <dhminch@gmail.com>
2022-01-15 09:41:58 -0500

    Add more ratelimiting to configuration

--- Configuration.py
+++ Configuration.py
@@ -25,7 +25,9 @@ class Configuration:
             
         self.logs = config_obj["logs"]
         self.cellphone = config_obj["cellphone"]
-        self.ratelimit = config_obj["ratelimit"]
+        self.limits_host_cooldown = config_obj["limits"]["host_cooldown"]
+        self.limits_max_per_hour = config_obj["limits"]["max_per_hour"]
+        self.limits_max_per_day = config_obj["limits"]["max_per_day"]
         self.twilio_messagingservice_sid = config_obj["twilio"]["messagingservice_sid"]
         self.twilio_account_sid = config_obj["twilio"]["account_sid"]
         self.twilio_auth_token = config_obj["twilio"]["auth_token"]
