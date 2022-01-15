3a8288554f4f04411ed12aa28819c14bbac41d3b
dhminch <dhminch@gmail.com>
2022-01-15 15:01:21 -0500

    Refactored log monitoring to a separate class

---
+++ LogMonitor.py
@@ -0,0 +1,86 @@
+
+import io
+import os
+import time
+
+import debug
+import entry_processors
+
+ENTRY_PROCESSORS = [
+    entry_processors.entry_processor_ssh_login,
+    entry_processors.entry_processor_sudo,
+    entry_processors.entry_processor_pkexec,
+    entry_processors.entry_processor_pfsense_web_login,
+    entry_processors.entry_processor_esxi_web_login,
+    entry_processors.entry_processor_tty_login,
+]
+
+class LogMonitor:
+
+    class LogFile:
+
+        def __init__(self, file, handle, inode):
+            self.file = file
+            self.handle = handle
+            self.inode = inode
+
+    def __init__(self, config, dispatcher):
+        self.dispatcher = dispatcher
+        self.config = config
+        self.logs_data = []
+
+    def monitor(self):
+        self.init_logs()
+        while True:
+            debug.log("Sleeping for {} seconds".format(self.config.monitor_frequency))
+            time.sleep(self.config.monitor_frequency)
+            self.process_logs()
+            self.dispatcher.send_alarms(self.config)
+
+    def init_logs(self):
+        debug.log("Initalizing logs")
+        for log in self.config.logs:
+            debug.log("Initalizing log file {}".format(log))
+            handle = open(log, "r", encoding='utf-8')
+            handle.seek(0, io.SEEK_END)
+            inode = os.stat(log).st_ino
+            logdata = LogMonitor.LogFile(log, handle, inode)
+            self.logs_data.append(logdata)
+
+    def check_entry_for_alarms(self, entry):
+        alarms = []
+
+        for processor in ENTRY_PROCESSORS:
+            debug.debug("Trying processor {}".format(processor.__name__))
+            alarm = processor(entry)
+            if alarm:
+                alarms.append(alarm)
+
+        return alarms
+
+    def process_logs(self):
+        for log_data in self.logs_data:
+            debug.log("Processing log file {}".format(log_data.file))
+            self.process_log(log_data.handle)
+
+            inode_on_disk = os.stat(log_data.file).st_ino
+            if log_data.inode != inode_on_disk:
+                debug.log("File inode for {} changed from {} to {}, processing file at new inode".format(
+                    log_data.file, log_data.inode, inode_on_disk
+                ))
+                log_data.handle.close()
+                log_data.handle = open(log_data.file, "r", encoding='utf-8')
+                log_data.inode = inode_on_disk
+                self.process_log(log_data.handle)
+
+    def process_log(self, log_handle):
+        alarms = []
+        for entry in log_handle:
+                entry = entry.strip()
+                debug.debug("Processing entry {}".format(entry))
+                if entry == "":
+                    continue
+                if entry_processors.ignore_entry(entry):
+                    continue
+                alarms.extend(self.check_entry_for_alarms(entry))
+        self.dispatcher.load_alarms(alarms)
--- syslog-sms.py
+++ syslog-sms.py
@@ -1,52 +1,16 @@
 
+import os.path
+
 from Alarm import AlarmDispatcher
 from Configuration import Configuration
-import debug
-import entry_processors
-
-ENTRY_PROCESSORS = [
-    entry_processors.entry_processor_ssh_login,
-    entry_processors.entry_processor_sudo,
-    entry_processors.entry_processor_pkexec,
-    entry_processors.entry_processor_pfsense_web_login,
-    entry_processors.entry_processor_esxi_web_login,
-    entry_processors.entry_processor_tty_login,
-]
-
-def generate_alarm_for_entry(entry):
-    alarms = []
-
-    for processor in ENTRY_PROCESSORS:
-        debug.debug("Trying processor {}".format(processor.__name__))
-        alarm = processor(entry)
-        if alarm:
-            alarms.append(alarm)
-
-    return alarms
-
-def process_logs(logs):
-    alarms = []
-
-    for file in logs:
-        debug.log("Processing log file {}".format(file))
-        with open(file, "r", encoding='utf-8') as handle:
-            for entry in handle:
-                entry = entry.strip()
-                debug.debug("Processing entry {}".format(entry))
-                if entry == "":
-                    continue
-                if entry_processors.ignore_entry(entry):
-                    continue
-                alarms.extend(generate_alarm_for_entry(entry))
-
-    return alarms
+from LogMonitor import LogMonitor
 
 def main():
-    config = Configuration("config.json")
-    alarms = process_logs(config.logs)
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    config = Configuration(os.path.join(__location__, "config.json"))
     dispatcher = AlarmDispatcher()
-    dispatcher.load_alarms(alarms)
-    dispatcher.send_alarms()
+    logmonitor = LogMonitor(config, dispatcher)
+    logmonitor.monitor()
 
 if __name__ == "__main__":
     main()
