9e16763d2f19bf2d47ffd6566c4cb2ea5d1a897d
dhminch <dhminch@gmail.com>
2024-01-27 19:16:03 -0500

    Add iDRAC to list of entry processors

--- LogMonitor.py
+++ LogMonitor.py
@@ -14,7 +14,8 @@ ENTRY_PROCESSORS = [
     entry_processors.entry_processor_esxi_web_login,
     entry_processors.entry_processor_tty_login,
     entry_processors.entry_processor_openvpn_login,
-    entry_processors.entry_processor_omv_web_login
+    entry_processors.entry_processor_omv_web_login,
+    entry_processors.entry_processor_idrac_login
 ]
 
 class LogMonitor:
