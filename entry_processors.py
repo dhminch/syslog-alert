a7c60c0c5e245dfb301d8a854722e137ddef856d
dhminch <dhminch@gmail.com>
2025-07-27 15:01:00 -0400

    Improve unifi regex

--- entry_processors.py
+++ entry_processors.py
@@ -17,7 +17,7 @@ RE_TTY_LOGIN = re.compile(r"LOGIN ON (\S+) BY (\S+)")
 RE_OPENVPN_LOGIN = re.compile(r"user '(\S+)' authenticated")
 RE_WIREGUARD_LOGIN = re.compile(r"WireGuard: New connection - Interface: (\S+), Peer: (\S+), Endpoint: (\S+):(\S+)")
 RE_OMV_WEB_LOGIN = re.compile(r"Authorized login from ([a-fA-F0-9:.]+) \[username=(\S+), user-agent=([^\]]+)]")
-RE_UNIFI_WEB_LOGIN = re.compile(r"(\S+) opened UniFi Network via the web. UNIFICategory=System UNIFIsubCategory=Admin Activity admin_ip=([0-9.]+)")
+RE_UNIFI_WEB_LOGIN = re.compile(r"=(\S+) opened UniFi Network via the web. UNIFICategory=System UNIFIsubCategory=Admin Activity admin_ip=([0-9.]+)")
 RE_IDRAC_LOGIN = re.compile(r".*USR0030, Message: Successfully logged in using (\S+), from (\S+) and (\S+)")
 
 RE_IGNORE_ENTRIES = [
